3c62a94c8be365c4fc8e652f7d069bdef06eacdf
Esther O <64683541+esther-cs@users.noreply.github.com>
2021-06-25 16:22:32 +0800

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  
+  int i = 0;
+  int square = 0;
+  
+  // Write a while loop here:
+  while (i<10){
+    std::cout<<i<<"   "<<square;
+    std::cout<<"\n";
+    i++;
+    square=i*i;
+  }
+  
